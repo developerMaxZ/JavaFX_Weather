9c216c0f47a983bde8c4532cf425337d698b40a9
developerMaxZ <117778495+developerMaxZ@users.noreply.github.com>
2022-11-19 06:21:00 +0500

    Update HelloApplication.kt

--- src/main/kotlin/com/example/javafx_weather/HelloApplication.kt
+++ src/main/kotlin/com/example/javafx_weather/HelloApplication.kt
@@ -1,3 +1,8 @@
+/*
+ * These projects were created for my training.
+ * Please do not use it as reliable information.
+ * Sincerely, M.Zazulin.
+ */
 package com.example.javafx_weather
 
 import javafx.application.Application
